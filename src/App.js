58107f9f42bda0bb8fb4744dde294b34d36ff2d9
Omar Abuhadid <omrs2002@yahoo.com>
2022-02-07 17:02:06 +0300

    call service

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employees: []
+    }
+  }
+
+  componentDidMount() {
+    const url = "https://localhost:7094/api/Employee";
+    fetch(url)
+    .then(response => response.json())
+    .then(json =>this.setState({ employees: json }))
+  }
+
+  render() {
+    const { employees } = this.state;
+    console.log("employees:",employees);
+    return (
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">Employees Details</h1>
+        </div>
+        {employees.map((emp) => (
+          <div className="card" key={emp.id}>
+            <div className="card-header">
+              #{emp.id} {emp.fullName}
+            </div>
+            <div className="card-body">
+              <p className="card-text">{emp.bio}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
-
 export default App;
