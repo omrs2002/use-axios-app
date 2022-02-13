c024b716c93242971267eae4732342c875b736df
Omar Abuhadid <omrs2002@yahoo.com>
2022-02-13 16:22:17 +0300

    Using redux with 2 counters

--- src/index.js
+++ src/index.js
@@ -6,8 +6,14 @@ import Posts  from './components/Posts' ;
 import Contr from './components/Countr';
 import FuncComp from './components/FuncComp';
 import ContactManager from './components/ContactManager'
-
 import reportWebVitals from './reportWebVitals';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { connect } from 'react-redux';
+
+import Counter from './components/reduxCounter'
+
 
 class HelloClass extends React.Component {
   
@@ -26,24 +32,109 @@ function Hello(props) {
 }
 const contacts = ["James Smith", "Thomas Anderson", "Bruce Wayne"];
 
+// Action creator
+function incrementCounter(num) {
+  return { type: 'INCREMENT', num: num }
+}
+
+function decrementCounter(num) {
+  return { type: 'DECREMENT', num: num }
+}
+
+const initialState = {count: 0};
+
+
+// Reducer function
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return { count: state.count + action.num };
+      case 'DECREMENT':
+      return { count: state.count - action.num };
+    default:
+      return state;
+  }
+}
+
+// Reducer2 function
+function reducer2(state = initialState, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return { count: state.count + action.num };
+    default:
+      return state;
+  }
+}
+
+function CounterRed(props) {
+  function handleClick() {
+    props.incrementCounter(1);
+  }
+  function handleDecClick() {
+    props.decrementCounter(1);
+  }
+
+    return <div>
+    <p>{props.count}</p>
+    <button  className="btn btn-primary btn-sm" onClick={handleClick}>Increment</button>
+    <span> </span>
+    <button className="btn btn-warning btn-sm" onClick={handleDecClick}>Decrement</button>
+    </div>
+}
+
+function mapStateToProps(state) {
+  return {
+    count: state.count
+  };
+}
+const mapDispatchToProps = {
+  incrementCounter,decrementCounter
+}
+
+const store = createStore(reducer);
+const store2 = createStore(reducer2);
+const CounterRedux = connect(mapStateToProps, mapDispatchToProps)(CounterRed);
+
+//#endregion
+
+
 ReactDOM.render(
 
   <React.StrictMode>
     <div className='row'>
       <div className='col-md-6'> 
       <Hello name="React!" />
+      <hr/>
   <HelloClass name="React hello class!" />
+  <hr/>
   <FuncComp></FuncComp>
+  <br/>
+  <hr/>
+  <center >
+      <h3>Redux Counter</h3>
+      <Provider store={store}>
+              <CounterRedux/>
+            </Provider>
+              <hr/>
+              
+              <Provider store={store2}>
+          <Counter/>
+        </Provider>
+              <hr/>
+              <Contr />
+  </center>
       </div>
       <div className='col-md-6'>
         <App />
+        <hr/>
          <Posts />
       </div>
     </div>
     
     <div className='row'>
       <div className='col-md-6'> 
-      <Contr />
+   
+      
       </div>
       <div className='col-md-6'> 
      <h1>Contact Manager</h1>
@@ -53,9 +144,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
